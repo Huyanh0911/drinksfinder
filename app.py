7f3cac7bd0f3676f90f6ceb312369955ef33e207
unknown <stephh9830@gmail.com>
2019-05-12 01:03:28 +0700

    cafe

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template,request,redirect
+app = Flask(__name__)
+from data import cafe_ndt
+
+@app.route('/all_cafe')
+def index():
+    all_cafe=cafe_ndt.find()
+    return render_template('allcafe.html',all_cafe=all_cafe)
+@app.route('/find',methods=["GET","POST"])
+def find():
+    if request.method =='GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        form = request.form
+        a=(form["add"])
+ 
+        
+        findcafe=cafe_ndt.find({"Quan":a})
+
+    
+        
+        return render_template('list1.html',findcafe=findcafe)
+
+    
+
+
+    
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+ 
